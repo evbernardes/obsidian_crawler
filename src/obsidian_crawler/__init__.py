dcd918596ffe68716c6956de9f1680af68170975
Evandro Bernardes <evbernardes@gmail.com>
2026-07-27 17:02:59 +0200

    Add MarkdownBlock to module exports for improved accessibility

--- src/obsidian_crawler/__init__.py
+++ src/obsidian_crawler/__init__.py
@@ -1,9 +1,11 @@
+from .block import MarkdownBlock
 from .note import ObsidianNote
 from .parsers import fuse_blocks, fuse_content, parse_blocks, parse_content
 from .query import ObsidianQuery
 from .vault import ObsidianVault
 
 __all__ = [
+    "MarkdownBlock",
     "ObsidianNote",
     "ObsidianQuery",
     "ObsidianVault",
